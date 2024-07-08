293147ce74ccac1128c7d65401667f56cfbdb36b
Dhruv Shandilya <112193252+FANTOM200@users.noreply.github.com>
2024-07-08 20:43:02 +0530

    Add files via upload

---
+++ Assignment-2.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#HELLO
+
+
+# In[2]:
+
+
+a=10
+
+
+# In[3]:
+
+
+b='10'
+c=int(b)
+d=a+c
+print(d)
+
+
+# In[4]:
+
+
+my_list = [1, 2, 3, 4, 5]
+sub_list=my_list[1:3]
+print(sub_list)
+
+
+# In[7]:
+
+
+#What is a complex number in mathematics
+#A complex number having one real part and one imaginery part
+num=3+2j
+type(num)
+
+
+# In[9]:
+
+
+
+age=25
+
+
+# In[11]:
+
+
+price=9.99
+type(price)
+
+
+# In[12]:
+
+
+name='Dhruv Shandilya'
+print(name)
+
+
+# In[14]:
+
+
+s='HELLO WORLD'
+print(s[6:])
+
+
+# In[15]:
+
+
+is_student=True
+print(is_student)
+
+
+# In[ ]:
+
+
+
+
